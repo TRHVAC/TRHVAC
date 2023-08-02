e65a2ed67e618085d3d541114986f01f8f7488e6
Jordan Lee <67548512+RomanticCoder@users.noreply.github.com>
2023-08-02 19:11:57 -0400

    page routing (home, service, booking pages)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Link from "next/link";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
+// blueprint of the whole project
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ul className="flex gap-3 bg-slate-400 p-3">
+        <Link href="/">Home</Link>
+        <Link href="/service">Service</Link>
+        <Link href="/booking">Booking</Link>
+      </ul>
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ pages/booking/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import Card from "../../components/card";
+
+export default function Home() {
+  return (
+    <div>
+      booking
+      <Card />
+    </div>
+  );
+}
