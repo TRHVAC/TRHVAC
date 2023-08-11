6f4b11b0b2f61d283bcf7f1bf790d68b1b3a304b
jang ssau <seowoojang05@gmail.com>
2023-08-11 00:27:13 -0400

    head tag update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,12 +3,13 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <div className="flex gap-3 bg-white p-3">
+      <div className=" flex gap-3 bg-white p-3">
         <p>hihi</p>
 
       </div>
-      <div>
+      <div className="top-nav">
         <ul className="flex gap-3 bg-slate-100 p-3">
+          <Link href='/'>TR HVAC</Link>
           <Link href="/">Home</Link>
           <Link href="/service">Service</Link>
           <Link href="/booking">Booking</Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="h-screen">main</div>;
+  return (
+      <>
+        <Head>
+          <title>TR HVAC | Heating & Cooling</title>
+          <meta name="description" content="TR HVAC offers professional heating and air conditioning services in Toronto" />
+          <meta content="heating, cooling, hvac, ventilation, air conditioning, tr" name="keywords" />
+        </Head>
+        <div className="h-screen">main</div>  
+      </>
+    );
 }
