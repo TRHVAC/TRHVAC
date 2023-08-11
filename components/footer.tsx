77b426ba06c30818c870ef5fdfc54dccdb8854c4
Minyoung Seol <seol.ca06@gmail.com>
2023-08-11 01:10:46 -0400

    footer initial commit

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 
 export default function Footer() {
-  return <div className="flex gap-3 bg-slate-400 p-3">Footer section</div>;
+  return (
+
+    <>
+      <div className="flex gap-3 bg-slate-400 p-3">Footer section</div>
+    </>
+    
+  )
+  
+  
   
 }
