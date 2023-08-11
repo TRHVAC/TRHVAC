2f9ac89ebefc084f5a61da0c885461f1728ae80a
jang ssau <seowoojang05@gmail.com>
2023-08-11 15:46:28 -0400

    svg test

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,30 @@
 import Link from "next/link";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default function Navbar() {
   return (
     <>
-      <div className=" flex gap-3 bg-white p-3">
+      <div className="flex bg-white p-3">
         <p>hihi</p>
 
       </div>
-      <div className="top-nav">
-        <ul className="flex gap-3 bg-slate-100 p-3">
+      <div className="top-nav flex gap-3 bg-slate-100 p-3">
+        <div>
           <Link href='/'>TR HVAC</Link>
+        </div>
+        <ul className="flex gap-3">
           <Link href="/">Home</Link>
           <Link href="/service">Service</Link>
           <Link href="/booking">Booking</Link>
         </ul>
+        <div className="emergency-box text-white">
+          {/* <FontAwesomeIcon icon={faCalendar as IconProp} />
+          <FontAwesomeIcon icon="fa-solid fa-phone" /> */}
+          <p>Emergency 24/7</p>
+          <p>(647) 522-5652</p>
+        </div>
       </div>
     </>
   );
