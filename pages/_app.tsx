75ee18dfce44b43af35d277a7309f0f10f784d81
jjaykim <dnglglmm@gmail.com>
2023-08-14 15:33:46 -0400

    implemented layout file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,30 +4,15 @@ import '../styles/globals.css';
 
 import Navbar from '@components/navbar';
 import Footer from '@components/footer';
+import Layout from '@components/layout';
 
 // blueprint of the whole project
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>TR HVAC | Heating & Cooling</title>
-        <meta
-          name="description"
-          content="TR HVAC offers professional heating and air conditioning services in Toronto"
-        />
-        <meta
-          content="heating, cooling, hvac, ventilation, air conditioning, tr"
-          name="keywords"
-        />
-      </Head>
-
-      <div className="h-screen max-w-7xl mx-auto">
-        <Navbar />
-
+      <Layout>
         <Component {...pageProps} />
-
-        <Footer />
-      </div>
+      </Layout>
     </>
   );
 }
