03ab98f04304266b830e9d8eaa13c8479a3bfead
Jordan Lee <67548512+RomanticCoder@users.noreply.github.com>
2023-08-28 13:04:04 -0400

    time input UI modified

--- pages/booking/index.tsx
+++ pages/booking/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import Head from 'next/head';
 import Image from 'next/image';
 import Map from '@components/map';
@@ -10,12 +11,50 @@ import Hero from '@components/hero';
 import Link from 'next/link';
 import {MapIcon} from '@components/icons/nav/MapIcon';
 import {PhoneIconBook} from'@components/icons/booking/PhoneIconBook';
+=======
+import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+import useMutation from "@libs/client/useMutation";
+import Hero from "@components/hero";
+import { TR_CONTACT_INFO } from "@utils/constants";
+import BookingPerson from "@public/booking/booking-person.jpg";
+
+export const bookingDetails = (iconColor: string) => {
+  return (
+    <>
+      <div>Monday - Saturday</div>
+
+      <div>09:00 am - 07:00 pm</div>
+
+      <Link
+        href={TR_CONTACT_INFO["Phone"].ref}
+        className="flex items-center gap-2"
+      >
+        {TR_CONTACT_INFO["Phone"].icon(iconColor)}
+        {TR_CONTACT_INFO["Phone"].title}
+      </Link>
+
+      <Link
+        href={TR_CONTACT_INFO["Address"].ref}
+        className="flex items-center gap-2"
+      >
+        {TR_CONTACT_INFO["Address"].icon(iconColor)}
+        {TR_CONTACT_INFO["Address"].title}
+      </Link>
+    </>
+  );
+};
+>>>>>>> Stashed changes
 
 interface BookingForm {
   email: string;
   name: string;
   type: string[];
   date: Date;
+  time: string[];
   address: string;
   city: string;
   postal: string;
@@ -33,7 +72,8 @@ interface MutationResult {
 }
 
 const Booking: NextPage = () => {
-  const [book, { loading, data, error }] = useMutation<MutationResult>('/api/booking');
+  const [book, { loading, data, error }] =
+    useMutation<MutationResult>("/api/booking");
   const {
     register,
     handleSubmit,
@@ -54,6 +94,7 @@ const Booking: NextPage = () => {
 
       <div className="flex flex-row gap-10 justify-center items-center my-10">
         {/* left text */}
+<<<<<<< Updated upstream
         <div className="flex flex-row">
           <div className="flex flex-col gap-1">
             <h2 className="text-2xl text-tr-blue font-semibold mb-4">We are here for you</h2>
@@ -70,6 +111,33 @@ const Booking: NextPage = () => {
               <span className='flex gap-1'><MapIcon/>L3P6Y5, North York, ON</span>
             </div>
             <div className='py-4'><Image src="/../public/booking/booking-person.jpg" alt="image of a person" width="180" height="250" /></div>
+=======
+        <div className="flex flex-col">
+          <h2 className="text-2xl text-tr-blue font-semibold mb-4">
+            We are here for you
+          </h2>
+
+          <div className="flex flex-col gap-1 text-tr-gray">
+            <div className="text-tr-blue font-semibold whitespace-pre-line">
+              Call{" "}
+              <Link className="text-tr-red" href={TR_CONTACT_INFO["Phone"].ref}>
+                {TR_CONTACT_INFO["Phone"].title}
+              </Link>
+              {"\n"}
+              or <span className="text-tr-skyBlue">book online</span> here.
+            </div>
+
+            {bookingDetails("#676661")}
+          </div>
+
+          <div className="py-4">
+            <Image
+              src={BookingPerson}
+              alt="image of a person"
+              placeholder="blur"
+              width={180}
+            />
+>>>>>>> Stashed changes
           </div>
         </div>
 
@@ -85,10 +153,10 @@ const Booking: NextPage = () => {
               <div className="rounded-md relative flex flex-col w-full items-center shadow-sm gap-2">
                 <div className="flex flex-row gap-2 w-full">
                   <input
-                    {...register('name', {
-                      required: 'user name is required',
+                    {...register("name", {
+                      required: "user name is required",
                       minLength: {
-                        message: 'the user name should be longer than 5 chars',
+                        message: "the user name should be longer than 5 chars",
                         value: 2,
                       },
                     })}
@@ -99,7 +167,7 @@ const Booking: NextPage = () => {
                   />
 
                   <select
-                    {...register('type', {})}
+                    {...register("type", {})}
                     className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300  shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                   >
                     <option value="" disabled>
@@ -112,26 +180,36 @@ const Booking: NextPage = () => {
                 </div>
                 <div className="flex flex-row gap-2 w-full">
                   <input
-                    {...register('date', {})}
+                    {...register("date", {})}
                     type="date"
                     required={false}
                     className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300  shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                   />
                   <select
-                    {...register('type', {})}
+                    {...register("type", {})}
                     className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300  shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                   >
                     <option value="" disabled>
-                      Preffered date
+                      Preffered Time
                     </option>
-                    <option value="option1">option1</option>
-                    <option value="option2">option2</option>
+                    <option value="9-10">9:00am - 10:00am</option>
+                    <option value="10-11">10:00am - 11:00am</option>
+                    <option value="11-12">11:00am - 12:00pm</option>
+                    <option value="12-1">12:00pm - 1:00pm</option>
+                    <option value="1-2">1:00pm - 2:00pm</option>
+                    <option value="2-3">2:00pm - 3:00pm</option>
+                    <option value="3-4">3:00pm - 4:00pm</option>
+                    <option value="4-5">4:00pm - 5:00pm</option>
+                    <option value="5-6">5:00pm - 6:00pm</option>
+                    <option value="6-7">6:00pm - 7:00pm</option>
+                    <option value="7-8">7:00pm - 8:00pm</option>
+                    <option value="8-9">8:00pm - 9:00pm</option>
                     <option value="other">other</option>
                   </select>
                 </div>
                 <div className="flex flex-row gap-2 w-full">
                   <input
-                    {...register('address', {})}
+                    {...register("address", {})}
                     type="text"
                     required={false}
                     placeholder="Street Address"
@@ -140,7 +218,7 @@ const Booking: NextPage = () => {
                 </div>
                 <div className="flex flex-row gap-2 w-full">
                   <input
-                    {...register('city', {})}
+                    {...register("city", {})}
                     type="text"
                     required={false}
                     placeholder="City"
@@ -148,7 +226,7 @@ const Booking: NextPage = () => {
                   />
 
                   <input
-                    {...register('postal', {})}
+                    {...register("postal", {})}
                     type="text"
                     required={false}
                     placeholder="Postal Code"
@@ -157,7 +235,7 @@ const Booking: NextPage = () => {
                 </div>
                 <div className="flex flex-row gap-2 w-full">
                   <input
-                    {...register('note', {})}
+                    {...register("note", {})}
                     type="text"
                     required={false}
                     placeholder="Commentss"
@@ -166,11 +244,11 @@ const Booking: NextPage = () => {
                 </div>
               </div>
               <button className="w-96 bg-blue-500 hover:bg-blue-600 text-white  px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 focus:outline-none py-3 text-base">
-                {loading ? 'Loading...' : 'Book Now'}
+                {loading ? "Loading..." : "Book Now"}
               </button>
 
               {/* tell that booking is done. => show booking info */}
-              {data && 'booking done. our team will contact you'}
+              {data && "booking done. our team will contact you"}
             </form>
           </div>
         </div>
